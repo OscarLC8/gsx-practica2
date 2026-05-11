f4539061bba64fd5722c5b38cb19a9d51ce958a5
Oscar <oscar.lopezc@estudiants.urv.cat>
2026-05-11 16:27:39 +0200

    Setmana 11: Infraestructura com a codi i CI/CD

---
+++ terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "missatge_exit" {
+  value = "La infraestructura d'Nginx i Backend s'ha creat correctament amb Terraform!"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "docker_username" {
+  description = "L'usuari de Docker Hub on hi ha les imatges"
+  type        = string
+  default     = "oscarlopezgsx" # Posa aquí el teu usuari real!
+}
