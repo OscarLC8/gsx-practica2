d910538a4fa1d5a4a8723c458d960e3c3ee80307
Oscar <oscar.lopezc@estudiants.urv.cat>
2026-05-14 21:50:28 +0200

    Neteja arxius

--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "docker_username" {
-  description = "L'usuari de Docker Hub on hi ha les imatges"
-  type        = string
-  default     = "oscarlopezgsx" # Posa aquí el teu usuari real!
-}
